d71a147bcc9620e692ae4d7b23130aed4a9d141d
Thomas <ThomasTail@users.noreply.github.com>
2020-01-28 09:17:07 -0500

    Update configV5.py

--- configV5.py
+++ configV5.py
@@ -1,14 +1,21 @@
 #email server
-server = ''
+server = 'mail.cock.li'
 
 #email server port
-port = 
+port = 587
 
 #password to all emails
-clientpass = ""
+clientpass = "password"
 
 #your emails
-clients = ['']
+clients = ['marshallking061@cock.li',
+          'marshallking061@airmail.cc',
+          'marshallking061@420blaze.it',
+          'marshallking061@cumallover.me',
+          'marshallking061@goat.si',
+          'marshallking061@horsefucker.org',
+          'marshallking061@national.shitposting.agency',
+          'marshallking061@cock.lu']
            
 supported_carriers = ['att', '@txt.att.net',
 		      'cellone', '@sms.cellonenation.net',
